Backprop fixes
Seems like it might actually work now.

functions.py
@@ -48,8 +48,8 @@ class Dense:
     def backward(self, da, cache=None):
         cache = self.cache if cache is None else cache
         dZ = da * self.dactivation(cache.T)
-        self.dw += 1/self.a_prev.shape[0] * np.dot(self.a_prev, dZ).T
-        self.db += 1/self.a_prev.shape[0] * np.sum(dZ, keepdims=True)
+        self.dw = 1/self.a_prev.shape[0] * np.dot(self.a_prev, dZ).T
+        self.db = 1/self.a_prev.shape[0] * np.sum(dZ, keepdims=True)
         return np.dot(dZ, self.w)
 
     def update_params(self, grad_clip=None):

lstm.py
@@ -54,11 +54,13 @@ class LSTM:
             targets: list of labels with the same shape as preds
             grad_check: if True, cell will store pre-clipped grads in cell.grads on param update
         """
-        d_loss = np.mean(self.dloss(preds, targets), axis=0).T
-        da_next = self.activation.backward(d_loss.T).T
+        d_loss = self.dloss(preds, targets)
+        das = [self.activation.backward(d_loss[t]).T for t in range(d_loss.shape[0])]
+        da_next = np.zeros_like((das[0]))
         dc_next = np.zeros_like((states[0]['c_out']))
         grads = self.cell.init_grads()
-        for state in reversed(states):
+        for state, da in zip(reversed(states), reversed(das)):
+            da_next += da
             da_next, dc_next, grad_adds = self.cell.backward(state, da_next, dc_next)
             for gate in ['c', 'u', 'o', 'f']:
                 grads[gate]['w'] += grad_adds[gate]['w']
@@ -118,7 +120,7 @@ class LSTM_unit:
         
         state = {}
         state['c_in'] = c_prev
-        state['z'] = np.vstack((x.T, a_prev))
+        state['z'] = np.vstack((a_prev, x.T))
 
         cache = {}
         for k, func in self.funcs.items():
@@ -130,22 +132,20 @@ class LSTM_unit:
         return state, cache
 
     def backward(self, state, da_next, dc_next):
-        dc_out = state['o'] * da_next + dc_next
+        dc_out = state['o'] * da_next * d_tanh(state['c_out']) + dc_next
         grads = self.init_grads()
 
         d = {}
-        d['c'] = state['c'] * (1 - state['c']) * state['u'] * dc_out
-        d['u'] = state['u'] * (1 - state['u']) * state['c_out'] * dc_out
-        d['o'] = state['o'] * (1 - state['o']) * state['c'] * da_next
+        d['c'] = (1 - state['c']**2) * state['u'] * dc_out
+        d['u'] = state['u'] * (1 - state['u']) * state['c'] * dc_out
+        d['o'] = state['o'] * (1 - state['o']) * tanh(state['c_out'])[0] * da_next
         d['f'] = state['f'] * (1 - state['f']) * state['c_in'] * dc_out
-
-        dz = np.zeros_like(state['z'])
+        da_in = np.zeros_like(da_next)
         for gate in ['c', 'u', 'o', 'f']:
-            dz += np.dot(self.params[gate]['w'].T, d[gate])
+            da_in += np.dot(self.params[gate]['w'].T[:self.hidden_dim,:], d[gate])
             grads[gate]['b'] = np.sum(d[gate], axis=1, keepdims=True)
             grads[gate]['w'] = np.dot(d[gate], state['z'].T)
-
-        da_in = dz[self.x_dim:]
+        
         dc_in = dc_out * state['f']
 
         return da_in, dc_in, grads

sandbox.py
@@ -1,7 +1,7 @@
 import numpy as np
 import time
 from lstm import LSTM
-
+from matplotlib import pyplot as plt
 
 def minibatch_gen(data, target, batch_size, shuffle=True):
     if shuffle:
@@ -11,26 +11,65 @@ def minibatch_gen(data, target, batch_size, shuffle=True):
     for i in range(1,num_batches+1):
         yield data[:,(i-1)*batch_size:i*batch_size,:], \
               target[:,(i-1)*batch_size:i*batch_size,:]
-time_steps = 10
-x_dim = 8
-hidden_dim = 8
-output_dim = 8
-n_examples = 8
-batch_size = 8
-x = np.random.randn(time_steps, n_examples, x_dim)
-y = np.random.randn(time_steps, n_examples, output_dim)
-x = np.ones((time_steps, n_examples, x_dim))
-y = np.ones((time_steps, n_examples, output_dim))
-net = LSTM(hidden_dim, x_dim, output_dim=output_dim, learning_rate=1e-8)
-for i in range(100000):
-    start = time.time()
-    loss = 0
-    for data, targets in minibatch_gen(x, y, batch_size):
-        loss += np.mean(net.epoch(data, targets))
-    print('Epoch {}: loss: {} time: {}'.format(i, loss, time.time() - start), end='\r', flush=True)
 
-print('\nEpoch {}: loss: {} time: {}'.format(i, loss, time.time() - start), end='\r', flush=True)
+def simplefunc():
+    time_steps = 10
+    x_dim = 8
+    hidden_dim = 8
+    output_dim = 8
+    n_examples = 2048
+    batch_size = 256
+    x = np.random.randn(time_steps, n_examples, x_dim)
+    y = np.random.randn(time_steps, n_examples, output_dim)
+    x = np.ones((time_steps, n_examples, x_dim))
+    y = np.ones((time_steps, n_examples, output_dim)) / 4.5
+    y[4:,:,:] = y[4:,:,:] * 3.6
+    net = LSTM(hidden_dim, x_dim, output_dim=output_dim, learning_rate=1e-5)
+    losses = []
+    for i in range(5000):
+        start = time.time()
+        loss = 0
+        for data, targets in minibatch_gen(x, y, batch_size):
+            loss += np.mean(net.epoch(data, targets))
+        losses.append(loss)
+        print('Epoch {}: loss: {} time: {}'.format(i, loss, time.time() - start), end='\r', flush=True)
+    
+    print('\nEpoch {}: loss: {} time: {}'.format(i, loss, time.time() - start), end='\r', flush=True)
+    plt.plot(losses)
+    plt.show()
 
+def courseratest():
+    np.random.seed(1)
+    x_dim = 3
+    n_examples = 10
+    time_steps = 7
+    hidden_dim = 5
+    
+    da = np.random.randn(5, 10, 4)
+    x = np.ones((time_steps, n_examples, x_dim))
+    for i in range(time_steps):
+        for j in range(n_examples):
+            for k in range(x_dim):
+                x[i,j,k] = np.random.randn()
+    from functions import xavier_init
+    net = LSTM(hidden_dim, x_dim)
+    states, caches, preds, ys = net.forward(x, np.zeros((time_steps, n_examples, 1)))
+    # print(states[-1]['z'])
+    # print(states[-1]['c_out'])
+    # print(states[-1]['f'])
+    # print(states[-1]['u'])
+    # print(states[-1]['o'])
+    da_next = np.zeros_like(da[:,:,0])
+    dc_next = np.zeros_like(states[0]['c'])
+    grads = net.cell.init_grads()
+    for t in reversed(range(4)):
+        da_next, dc_next, grad_adds = net.cell.backward(states[t], da[:,:,t] + da_next, dc_next)
+        for gate in ['c', 'u', 'o', 'f']:
+            grads[gate]['w'] += grad_adds[gate]['w']
+            grads[gate]['b'] += grad_adds[gate]['b']
+        print(grad_adds['f']['b'])
+if __name__ == "__main__":
+    simplefunc()
 
 # class Model:
 #     def __init__(layers, loss):

tests.py
@@ -6,7 +6,7 @@ import numpy as np
 from functions import Dense, L2_loss, d_sigmoid, d_tanh, sigmoid, tanh
 from lstm import LSTM, LSTM_unit
 
-TEST_COUNT = 10 # How many times most tests are ran
+TEST_COUNT = 2 # How many times most tests are ran
 def random_params():
     time_steps = np.random.randint(1, 10) * 10
     hidden_dim = np.random.randint(90, 110)
@@ -182,6 +182,7 @@ def get_num_grad(net, param, idx, delta, arr, targets):
 
 def test_lstm_grads():
     delta = 1e-5
+    np.random.seed(1)
     (time_steps, hidden_dim, output_dim,
      x_dim, n_examples, batch_size) = random_params()
     arr = np.random.randn(time_steps, 1, x_dim)